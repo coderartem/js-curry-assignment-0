2c323e1a972964c77ca34ddcada22fc6e8ffdf7c
unknown <coderartem@gmail.com>
2017-08-31 16:14:30 -0500

    initial commit

--- lib.js
+++ lib.js
@@ -42,10 +42,12 @@ const itemRepeater =
  * as an array of items
  */
 /*
-Using customers.reduce() method to iterate through customer objects, creating cart object for every customer and filling it with customer.name 
-and array of items in customer cart. Array of items in customer cart we are getting as return of reduce() iteration on return of  entries() 
-function (sending crntCustomer.shoppingList as an argument).  entries() function return array of arrays [itemName, count]. 
-Sending both values of those inner arrays as a parametr to itemRepeater() function we are getting array of items for crntCustomer as a return
+Using customers.reduce() method to iterate through customer objects, creating cart object for every customer by calling cart() function. 
+Sending current customer shoppingList object to entries() function that return array of arrays [itemName, count]. 
+Using reduce() method to iterate through inner arrays of returned array and sending both values of those inner arrays as a parameter to 
+itemRepeater() function that return array of 'itemName' 'count' number of times. Creating items list for current customer by adding those arrays 
+of particular items together. Putting that list of items as second parameter in cart() function along with crntCustomer.name as first parameter.
+Pushing returned cart() object to resultArray.
 */
 
 const constructCarts =
